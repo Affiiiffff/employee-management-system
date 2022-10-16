88bf553dc5d93dc9de86fb093bee170bd6d49a55
Afif Patel <afifpatel@hotmail.co.uk>
2022-10-16 15:36:07 +0100

    hi

--- db/schema.sql
+++ db/schema.sql
@@ -23,9 +23,8 @@ CREATE TABLE employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
+    REFERENCES roles(id),
     FOREIGN KEY (manager_id)
     REFERENCES employees(id)
     ON DELETE SET NULL
